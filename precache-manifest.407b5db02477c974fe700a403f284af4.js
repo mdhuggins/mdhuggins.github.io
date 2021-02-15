56adc3b59eed84fae489fcc26aa0d991991f5c6c
Matthew Huggins <hugginsm@mit.edu>
2021-02-14 23:05:34 -0500

    Updates

---
+++ precache-manifest.407b5db02477c974fe700a403f284af4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bf33a9e60ca11414be46e223de772cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b098bde92d268d72904",
+    "url": "/static/css/2.143f9196.chunk.css"
+  },
+  {
+    "revision": "999396b205f2507ca3d0",
+    "url": "/static/css/main.916141f1.chunk.css"
+  },
+  {
+    "revision": "6b098bde92d268d72904",
+    "url": "/static/js/2.480f858b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.480f858b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "999396b205f2507ca3d0",
+    "url": "/static/js/main.03b457f7.chunk.js"
+  },
+  {
+    "revision": "4be130620b223c1c9c69",
+    "url": "/static/js/runtime-main.1cbfc7dc.js"
+  },
+  {
+    "revision": "b6776d238c6163d8960678ca2b292cdb",
+    "url": "/static/media/fade.b6776d23.png"
+  }
+]);
